b1e181582b44848ddd69169c3687f38418563c7e
ppfuark <phfuark2007@gmail.com>
2025-04-10 17:03:38 -0300

    Class Based

---
+++ PWBE/Class Based - 10.04.2025/pato/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from .models import Pato
+from .serializers import PatoSerializer
+from rest_framework.pagination import PageNumberPagination
+# Create your views here.
+
+class PatoPaginacao(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'qnt_pagina'
+    max_page_size = 10
+
+class PatoListCreateAPIView(ListCreateAPIView):
+    queryset = Pato.objects.all()
+    serializer_class = PatoSerializer
+    pagination_class = PatoPaginacao ''
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        nome = self.request.query_params.get('nome')
+        if nome:
+            queryset = queryset.filter(nome__icontains=nome)
+        return 
+
+    
