31216c8dd52b74f15e0175fea3d775d8ab92a913
ppfuark <phfuark2007@gmail.com>
2025-04-29 16:05:58 -0300

    PWBE | Formativa
    ListCreateAPIView and RetrieveUpdateDestroyAPIView with Authentication and Authorization

--- PWBE/!Formativa/api/views.py
+++ PWBE/!Formativa/api/views.py
@@ -1,38 +1,67 @@
-from django.forms import ValidationError
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from .models import Subject, Teacher, RoomReservation
 from .serializers import SubjectSerializer, TeacherSerializer, RoomReservationSerializer
 
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 
+
+# ====================================== Subjects ============================================
+
 class SubjectListCreateAPIView(ListCreateAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
-    # permission_classes = [IsAuthenticated] 
     
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        name = self.request.query_params.get('name')
-        if name:
-            queryset = queryset.filter(name__icontains=name)
-        return queryset
-    
-    def create(self, serializer):
-        if serializer.validated_data['name'].strip() == "":
-            raise serializer.ValidationError("Name cannot be empty")
-        serializer.save()
+    # def get_permissions(self):
+    #     if self.request.method == 'GET':
+    #         return [IsAuthenticated()]
+    #     return [IsManager()]
         
 class SubjectRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
-    # permission_classes = [IsAuthenticated] 
     
-    def put(self, request, *args, **kwargs):
-        name = self.request.query_params.get('name')
-        if len(name.strip()) < 2:
-            raise ValidationError({"name": "Name must have at least 2 characters."})
-        return super().put(request, *args, **kwargs)
+    # def get_permissions(self):
+    #     return [IsManager()
+
+# ====================================== Subjects ============================================
+
+
+# ====================================== Teacher ============================================
+
+class TeacherListCreateAPIView(ListCreateAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
     
+    # def get_permissions(self):
+    #     if self.request.method == 'GET':
+    #         return [IsAuthenticated]
+    #     return [IsManager()]
+
+class TeacherRetriveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+    
+    # def get_permissions(self):
+    #     return [IsManager()]
+
+# ====================================== Teacher ============================================
+
+
+# ====================================== Teacher ============================================
+
+class RoomReservationListCreateAPIView(ListCreateAPIView):
+    queryset = RoomReservation.objects.all()
+    serializer_class = RoomReservationSerializer
+    
+    # def get_permissions(self):
+    #     if self.request.method == 'GET':
+    #         return [IsAuthenticated]
+    #     return [IsManager()]
+    
+class RoomReservationRetriveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    print("nn")
+    # def get_permissions(self):
+    #     return [IsManager()]
+
+
+# ====================================== Teacher ============================================
